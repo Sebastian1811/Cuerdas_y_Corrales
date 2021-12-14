721b8d5419e151b894c9d2a22a955acd0cc21cc0
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-12-14 13:49:35 -0500

    Poniendo imagenes

--- gui.py
+++ gui.py
@@ -3,9 +3,13 @@ import sys, pygame
 
 
 pygame.init()
-size = width, height = 800, 800
+size = width, height = 900, 900
 screen = pygame.display.set_mode((size))
-pygame.display.set_caption("Cuerdas o Corrales xd")
+pygame.display.set_caption("Cuerdas o Corrales ")
+morty = pygame.image.load("img/morty.png")
+morty = pygame.transform.scale(morty, (100, 100))
+robot = pygame.image.load("img/robot.png")
+robot = pygame.transform.scale(robot,(100,100))
 
 
 
@@ -13,5 +17,8 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             sys.exit()
+    
+    screen.blit(morty,(750,650))
+    screen.blit(robot,(750,780))
     pygame.display.update()
 
