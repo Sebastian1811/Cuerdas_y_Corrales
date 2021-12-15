57ec3947f5ebaa033e8bb8b0658ac1b09ac45fad
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-12-14 19:14:40 -0500

    pintarMapa

--- gui.py
+++ gui.py
@@ -1,15 +1,22 @@
 import sys, pygame
 from juego import *
 
+
+
+
 class gui:
     pygame.init()
-    size = width, height = 900, 900
+    size = width, height = 678, 1000
+    white = 255, 255, 255
+    red = 255, 0 , 0
     screen = pygame.display.set_mode((size))
     pygame.display.set_caption("Cuerdas o Corrales ")
     morty = pygame.image.load("img/morty.png")
     morty = pygame.transform.scale(morty, (100, 100))
     robot = pygame.image.load("img/robot.png")
     robot = pygame.transform.scale(robot,(100,100))
+    portal = pygame.image.load("img/portal.png")
+    portal = pygame.transform.scale(portal,(80,80))
     count = 1
     cuadros = None
     tablero = None
@@ -24,14 +31,14 @@ class gui:
         
         for i in range(filas):
             for j in range(columnas):
-                """print(self.cuadros[i][j].LineaArriba)
+                print(self.cuadros[i][j].LineaArriba)
                 print(self.cuadros[i][j].Puntaje)
-                print(self.cuadros[i][j].LineaAbajo)"""
+                print(self.cuadros[i][j].LineaAbajo)
                 self.coordenadas.append(self.cuadros[i][j].CoordenadaInicial)
                 self.puntajes.append(self.cuadros[i][j].Puntaje)
 
                 print()
-        print(self.matrizJuego)    
+        print(self.matrizJuego)
 
     def setMatrizJuego(self,filas,columnas):
         ancho = []
@@ -39,22 +46,73 @@ class gui:
             for j in range(columnas):
                 ancho.append(0)
             self.matrizJuego.append(ancho)
-            ancho = []  
+            ancho = [] 
+         
+
+    def pintarmapa(self):
+
+        print(len(self.matrizJuego))
+        x=0
+        y=0
+        for i in range(len(self.matrizJuego)):
+            a=i+1
+            if (a % 2) !=0:
+                for j in range(len(self.matrizJuego)):
+                    b = j+1
+                    if(b % 2) !=0:
+                        self.screen.blit(self.portal,[x+10,y+10])
+                        x+=96
+                    else:
+                        pygame.draw.rect(self.screen,self.white,[x,y,95,95],0)
+                        x+=96 
+            else:
+                for z in range(len(self.matrizJuego)):
+                    c = z+1
+                    if(c % 2) !=0:
+                        pygame.draw.rect(self.screen,self.white,[x,y,95,95],0)
+                        x+=96
+                    else:
+                        """F=96
+                        C=96
+                        for i in range(0,2):
+                            C=C+96
+                            for j in range(0,2):
+                              self.screen.blit(str(self.cuadros[i][j].Puntaje),(F,C))
+                              F=F+96  """ 
+                        pygame.draw.rect(self.screen,self.red,[x,y,95,95],0)
+                        x+=96                
+            x=0
+            y+=96
+
 
     def interfaz(self):
+        self.pintarmapa()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
                     sys.exit()
             if self.count:
-                self.setTablero(5,5)
-                self.count+=1        
-            self.screen.blit(self.morty,(750,650))
-            self.screen.blit(self.robot,(750,780))
-            pygame.display.update()
+                #self.setTablero(5,5)
+                self.count+=1      
+            self.screen.blit(self.morty,(580,720))
+            self.screen.blit(self.robot,(580,880))
+            pygame.display.flip()
+
+
 
 gu = gui()
-
 gu.setTablero(4,4)
-print(gu.coordenadas)
-print(gu.puntajes)
+gu.interfaz()
+"""print(gu.coordenadas)
+print(gu.puntajes)"""
+
+
+
+"""for fila in range(len(self.matrizJuego)):
+            for  columna in range(len(self.matrizJuego[fila])):
+                if self.matrizJuego[fila][columna] == 0:
+                    pygame.draw.rect(self.screen,self.white,[x,y,95,95],0)
+                    x+=96
+                if self.matrizJuego[fila+1][columna+1] == 0:
+                    self.screen.blit(self.portal,[x+10,y+10])
+                    x+=96""" 
